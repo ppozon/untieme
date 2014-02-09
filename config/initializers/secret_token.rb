97bbe84441d715cd446bd29b13920325570e7af1
Patty Pozon <ppozon@yahoo.com>
2014-02-08 18:32:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Untieme::Application.config.secret_key_base = '20829e49871de6777eabff5bf5729298fb52892fd75e5763109a6f488bd83e4588939c49bf1fc1a4d00275f10633b7a1b31de1333bf6abbe414de777b7f666a6'
